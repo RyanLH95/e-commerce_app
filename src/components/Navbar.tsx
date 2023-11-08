cd21422da48985c53785530a12014e88d7302f4e
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-11-08 13:57:14 +0000

    Add a 'lib' directory to src and create a 'utils.ts' file inside. Install clsx and tailwind-merg depencies and import inside utils.ts file, then import 'bgDesign' in layout.css file.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div
+      className="w-full h-20 border-b-[1px] 
+    border-b-zinc-500 bg-white"
+    >
+      <div>
+        <p>Logo</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
