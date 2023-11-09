9c7cda6b49fbba33274c4d10a803c5846efaa78b
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-11-09 11:16:51 +0000

    Create a 'navigation' const inside 'Navbar' function and give it an array with all components required for navigation. Add a '<Logo />' component inside <div> and give navigation a .map method and design using tailwind.css.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,34 @@
 import React from "react";
+import Logo from "./Logo";
+import Link from "next/link";
 
 const Navbar = () => {
+
+  const navigation =[
+    {_id:910, title: "Home", href: "/",},
+    {_id:911, title: "Trainers", href: "/trainers",},
+    {_id:912, title: "T-shirts", href: "/t-shirts",},
+    {_id:913, title: "Sweatshirts", href: "/sweatshirts",},
+    {_id:914, title: "Accessories", href: "/accessories",},
+  ];
   return (
     <div
-      className="w-full h-20 border-b-[1px] 
-    border-b-zinc-500 bg-white"
-    >
-      <div>
-        <p>Logo</p>
+      className="w-full h-20 border-b-[1px] border-b-zinc-500 bg-white">
+      <div className="max-w-screen-x1 mx-auto flex h-full items-center px-20 justify-between scroll-px-4 x1:px-0">
+        {/* Logo & */}
+        <Logo />
+        {/* Navigation */}
+        <ul className="hidden md:flex items-center gap-5 text-sm uppercase font-semibold">
+          {
+            navigation.map((item) => (
+              <Link href={item?.href}>
+                <li>{item?.title}</li>
+              </Link>
+            ))
+          }
+        </ul>
+        {/* icons */}
+        <p>Icons</p>
       </div>
     </div>
   );
